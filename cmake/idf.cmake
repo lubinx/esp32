f06533eff6950d9d9fe925f67023abb3660bdfc0
lubin <binidentity@gmail.com>
2022-11-13 02:58:08 +0800

    cmake cache variables

--- cmake/idf.cmake
+++ cmake/idf.cmake
@@ -3,44 +3,32 @@ if(__idf_env_set)
     return ()
 endif()
 
-if (NOT IDF_PATH)
-    set(IDF_PATH $ENV{IDF_PATH})
-endif()
 if (NOT IDF_TARGET)
-    set(IDF_TARGET "esp32s3")
-    message(STATUS "### variable IDF_TARGET is not set, default set to esp32s3")
+    set(IDF_TARGET "esp32s3" CACHE STRING "esp-idf build target")
+    message(STATUS "❓ variable IDF_TARGET is not set, default set to esp32s3")
 endif()
-
-if (NOT IDF_PATH)
-    set(IDF_PATH $ENV{IDF_PATH})
-endif()
-if (NOT IDF_PATH)
-    set(IDF_PATH "$ENV{HOME}/esp-idf")
-    message(STATUS "### variable IDF_PATH is not set, default set to $ENV{HOME}/esp-idf")
-endif()
-
-if (NOT IDF_ENV_PATH)
-    set(IDF_ENV_PATH $ENV{IDF_ENV_PATH})
-endif()
-if (NOT IDF_ENV_PATH)
-    message(STATUS "### variable IDF_ENV_PATH is not set, default set to $ENV{HOME}/.espressif")
-    set(IDF_ENV_PATH "$ENV{HOME}/.espressif")
-endif()
-
-set(IDF_TARGET ${IDF_TARGET} CACHE STRING "esp-idf build target")
-set(IDF_PATH ${IDF_PATH} CACHE STRING "esp-idf source path")
-set(IDF_ENV_PATH ${IDF_ENV_PATH} CACHE STRING "esp-idf environment tools install path")
-
 set_property(CACHE IDF_TARGET PROPERTY STRINGS esp32 esp32s2 esp32s3 esp32c3 esp32h2 esp32c2 esp32c6)
 
-if (DEFINED ENV{IDF_TARGET})
-    set(IDF_TARGET $ENV{IDF_TARGET})
+if (NOT IDF_PATH)
+    if (DEFINED ENV{IDF_PATH})
+        set(IDF_PATH $ENV{IDF_PATH})
+    else()
+        set(IDF_PATH "$ENV{HOME}/esp-idf")
+    endif()
+    set(IDF_PATH ${IDF_PATH} CACHE STRING "esp-idf source path")
+    message(STATUS "❓ variable IDF_PATH is not set, default set to $ENV{HOME}/esp-idf")
+elseif (DEFINED ENV{IDF_PATH} AND NOT ($ENV{IDF_PATH} STREQUAL ${IDF_PATH}))
+    message(STATUS "❌ IDF_PATH was changed since last build")
+    message(STATUS "✔️ clear cmake cache to fix\n\n")
+    message(FATAL_ERROR)
 endif()
-if (DEFINED ENV{IDF_PATH})
-    set(IDF_PATH $ENV{IDF_PATH})
-endif()
-if (DEFINED ENV{IDF_ENV_PATH})
+
+if (NOT IDF_ENV_PATH)
     set(IDF_ENV_PATH $ENV{IDF_ENV_PATH})
+endif()
+if (NOT IDF_ENV_PATH)
+    set(IDF_ENV_PATH "$ENV{HOME}/.espressif")
+    message(STATUS "❓ variable IDF_ENV_PATH is not set, default set to ${IDF_ENV_PATH}")
 endif()
 
 set(CMAKE_TOOLCHAIN_FILE ${IDF_PATH}/tools/cmake/toolchain-${IDF_TARGET}.cmake)
@@ -55,9 +43,28 @@ include(${IDF_CMAKE_PATH}/ldgen.cmake)
 include(${IDF_CMAKE_PATH}/utilities.cmake)
 include(${IDF_CMAKE_PATH}/version.cmake)
 
+if (IDF_VERSION AND NOT ($ENV{IDF_VERSION} STREQUAL "${IDF_VERSION}"))
+    message(STATUS "❌ IDF_VERSION was changed since last build")
+    message(STATUS "✔️ clear cmake cache to fix\n\n")
+else()
+    set(IDF_VERSION $ENV{IDF_VERSION} CACHE STRING "esp-idf version")
+endif()
+
+# if (NOT gvar)
+#     set(gvar "hello" CACHE STRING "")
+#     message("cache defined")
+
+#     set_property(CACHE gvar PROPERTY STRINGS "l1" APPEND)
+#     set_property(CACHE gvar PROPERTY STRINGS "l2" APPEND)
+# else()
+#     message(STATUS gvar)
+#     get_property(v CACHE gvar PROPERTY STRINGS)
+#     message (STATUS "  ${v}")
+# endif()
+# message(FATAL_ERROR "\n\nbreak")
+
 # function(foobar var)
 #     set(${var} "foobar" PARENT_SCOPE)
-#     set_property(GLOBAL PROPERTY gvar "FOOBAR set gvar")
 # endfunction()
 
 # function(foo)
@@ -76,7 +83,55 @@ include(${IDF_CMAKE_PATH}/version.cmake)
 #############################################################################
 # overrides component.cmake
 #############################################################################
-function(__component_get_requirements)
+function(__component_add component_dir prefix)
+    idf_build_get_property(component_targets __COMPONENT_TARGETS)
+    get_filename_component(abs_dir ${component_dir} ABSOLUTE)
+    get_filename_component(base_dir ${abs_dir} NAME)
+
+    if(NOT EXISTS "${abs_dir}/CMakeLists.txt")
+        message(FATAL_ERROR "Directory '${component_dir}' does not contain a component.")
+    endif()
+
+    set(component_name ${base_dir})
+    # The component target has three underscores as a prefix. The corresponding component library
+    # only has two.
+    set(component_target ___${prefix}_${component_name})
+
+    # If a component of the same name has not been added before If it has been added
+    # before just override the properties. As a side effect, components added later
+    # 'override' components added earlier.
+    if(NOT component_target IN_LIST component_targets)
+        if(NOT TARGET ${component_target})
+            add_library(${component_target} STATIC IMPORTED)
+        endif()
+        idf_build_set_property(__COMPONENT_TARGETS ${component_target} APPEND)
+    else()
+        message(FATAL_ERROR "break here")
+        __component_get_property(dir ${component_target} COMPONENT_DIR)
+        __component_set_property(${component_target} COMPONENT_OVERRIDEN_DIR ${dir})
+    endif()
+
+    set(component_lib __${prefix}_${component_name})
+    set(component_dir ${abs_dir})
+    set(component_alias ${prefix}::${component_name}) # The 'alias' of the component library,
+                                                    # used to refer to the component outside
+                                                    # the build system. Users can use this name
+                                                    # to resolve ambiguity with component names
+                                                    # and to link IDF components to external targets.
+
+    # Set the basic properties of the component
+    __component_set_property(${component_target} COMPONENT_LIB ${component_lib})
+    __component_set_property(${component_target} COMPONENT_NAME ${component_name})
+    __component_set_property(${component_target} COMPONENT_DIR ${component_dir})
+    __component_set_property(${component_target} COMPONENT_ALIAS ${component_alias})
+
+    __component_set_property(${component_target} __PREFIX ${prefix})
+
+    # Set Kconfig related properties on the component
+    __kconfig_component_init(${component_target})
+
+    # set BUILD_COMPONENT_DIRS build property
+    idf_build_set_property(BUILD_COMPONENT_DIRS ${component_dir} APPEND)
 endfunction()
 
 #############################################################################
@@ -130,7 +185,7 @@ function(__build_init)
     idf_build_set_property(SDKCONFIG "${CMAKE_SOURCE_DIR}/sdkconfig") # from PROJECT_DIR
     idf_build_set_property(SDKCONFIG_DEFAULTS "")
 
-    if("${IDF_TARGET}" STREQUAL "esp32" OR "${IDF_TARGET}" STREQUAL "esp32s2" OR "${IDF_TARGET}" STREQUAL "esp32s3")
+    if(("${IDF_TARGET}" STREQUAL "esp32") OR ("${IDF_TARGET}" STREQUAL "esp32s2") OR ("${IDF_TARGET}" STREQUAL "esp32s3"))
         idf_build_set_property(IDF_TARGET_ARCH "xtensa")
     elseif("${IDF_TARGET}" STREQUAL "linux")
         idf_build_set_property(IDF_TARGET_ARCH "")
@@ -139,10 +194,10 @@ function(__build_init)
     endif()
 
     # __build_get_idf_git_revision()
-    idf_build_set_property(COMPILE_DEFINITIONS "IDF_VER=\"$ENV{IDF_VERSION}\"")
+    idf_build_set_property(COMPILE_DEFINITIONS "IDF_VER=\"${IDF_VERSION}\"")
 
     # this is for build esp-idf components
-    idf_build_set_property(__PREFIX "idf")
+    idf_build_set_property(__PREFIX esp-idf)
 
     # search python: narrow to idfx.x_* should be only 1 result
     file(GLOB python_dir "${IDF_ENV_PATH}/python_env/idf${IDF_VERSION_MAJOR}.${IDF_VERSION_MINOR}_*")
@@ -161,12 +216,29 @@ function(__build_init)
 
     idf_build_set_property(__COMPONENT_REQUIRES_COMMON "${requires_common}")
 ####################
+
+    # cmake cache all directory of ${IDF_PATH}/components
+    if (NOT IDF_COMPONENT_DIRS_CACHE)
+        set(IDF_COMPONENT_DIRS_CACHE "${IDF_PATH}" CACHE STRING "")
+        file(GLOB dirs ${IDF_PATH}/components/*)
+
+        foreach(dir ${dirs})
+            if(IS_DIRECTORY ${dir})
+                __component_dir_quick_check(is_component ${dir})
+                if(is_component)
+                    set_property(CACHE IDF_COMPONENT_DIRS_CACHE PROPERTY STRINGS ${dir} APPEND)
+                endif()
+            endif()
+        endforeach()
+    endif()
+
+    message(STATUS "Building ESP-IDF components for target ${IDF_TARGET}")
 endfunction()
 
 # NOTE: build initialization
 __build_init()
 
-function(__gathering_idf_components_deps)
+function(IDF_build_application)
     # CMAKE_PROJECT_<VAR> only available after project()
     idf_build_set_property(PROJECT_NAME ${CMAKE_PROJECT_NAME})
     if (NOT CMAKE_PROJECT_VERSION)
@@ -180,25 +252,13 @@ function(__gathering_idf_components_deps)
         idf_build_set_property(COMPILE_DEFINITIONS "BOOTLOADER_BUILD=1" APPEND)
     endif()
 
-    message(STATUS "Building ESP-IDF components for target ${IDF_TARGET}")
+    idf_build_get_property(prefix __PREFIX)
 
-    file(GLOB component_dirs ${IDF_PATH}/components/*)
-    idf_build_get_property(prefix  __PREFIX)
-
+    get_property(component_dirs CACHE IDF_COMPONENT_DIRS_CACHE PROPERTY STRINGS)
     foreach(component_dir ${component_dirs})
-        if(IS_DIRECTORY ${component_dir})
-            __component_dir_quick_check(is_component ${component_dir})
-            if(is_component)
-                __component_add(${component_dir} ${prefix})
-            endif()
-        endif()
+        __component_add(${component_dir} ${prefix})
     endforeach()
 
-    # redirect call
-    ___component_get_requirements()
-endfunction()
-
-function(__kconfig_idf_components)
     idf_build_get_property(sdkconfig SDKCONFIG)
     idf_build_get_property(sdkconfig_defaults SDKCONFIG_DEFAULTS)
 
@@ -211,11 +271,6 @@ function(__kconfig_idf_components)
 
     # reset build components
     idf_build_unset_property(__BUILD_COMPONENT_TARGETS)
-endfunction()
-
-function(IDF_build_application)
-    __gathering_idf_components_deps()
-    __kconfig_idf_components()
 
     idf_build_get_property(component_targets __COMPONENT_TARGETS)
     foreach(component_target ${component_targets})
@@ -264,6 +319,7 @@ function(IDF_build_application)
                 ${build_component}
                 "-Wl,--no-whole-archive")
         else()
+            message(STATUS "######## Add link_library: ${build_component}")
             target_link_libraries(${project_elf} PRIVATE ${build_component})
         endif()
     endforeach()
@@ -287,6 +343,5 @@ endfunction()
 
 macro(IDF_buildroot)
     set(BOOTLOADER_BUILD 1)
-
     IDF_build_application()
 endmacro()
