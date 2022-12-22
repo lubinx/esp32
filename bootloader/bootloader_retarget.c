c816577f86138f468216e433647f5b602b611572
lubin <binidentity@gmail.com>
2022-12-22 23:09:57 +0800

    bootloader retarget

---
+++ bootloader/bootloader_retarget.c
@@ -0,0 +1,36 @@
+/*
+    libc retarget when bootloader
+        at this time, nothing is need to implemented
+*/
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#pragma GCC diagnostic ignored "-Wattribute-alias"
+
+static int syscall_not_implemented(struct _reent *r, ...)
+{
+    __errno_r(r) = ENOSYS;
+    return -1;
+}
+
+int _getpid_r(struct _reent *r)
+    __attribute__((alias("syscall_not_implemented")));
+
+int _close_r(struct _reent *r, int fd)
+    __attribute__((weak,alias("syscall_not_implemented")));
+
+int _fstat_r (struct _reent *r, int fd, struct stat *st)
+    __attribute__((weak,alias("syscall_not_implemented")));
+
+off_t _lseek_r(struct _reent *r, int fd, off_t size, int mode)
+    __attribute__((weak,alias("syscall_not_implemented")));
+
+ssize_t _read_r(struct _reent *r, int fd, void * dst, size_t size)
+    __attribute__((weak,alias("syscall_not_implemented")));
+
+int _kill_r(struct _reent *r, int pid, int sig)
+    __attribute__((alias("syscall_not_implemented")));
+
+ssize_t _write_r(struct _reent *r, int fd, const void * data, size_t size)
+    __attribute__((weak,alias("syscall_not_implemented")));
