48c6366001cb8e3825c34a1dd9bfd07654c45799
lubin <binidentity@gmail.com>
2023-03-08 19:51:00 +0800

    doc

--- esp_system/driver/esp32s3/include/mmu_ll.h
+++ esp_system/driver/esp32s3/include/mmu_ll.h
@@ -10,7 +10,49 @@
 
 #include "soc/extmem_reg.h"
 #include "soc/ext_mem_defs.h"
-#include "mmu_types.h"
+
+
+typedef enum {
+    MMU_MEM_CAP_EXEC  = BIT(0),
+    MMU_MEM_CAP_READ  = BIT(1),
+    MMU_MEM_CAP_WRITE = BIT(2),
+    MMU_MEM_CAP_32BIT = BIT(3),
+    MMU_MEM_CAP_8BIT  = BIT(4),
+} mmu_mem_caps_t;
+
+/**
+ * MMU Page size
+ */
+typedef enum {
+    MMU_PAGE_8KB = 0x2000,
+    MMU_PAGE_16KB = 0x4000,
+    MMU_PAGE_32KB = 0x8000,
+    MMU_PAGE_64KB = 0x10000,
+} mmu_page_size_t;
+
+/**
+ * MMU virtual address type
+ */
+typedef enum {
+    MMU_VADDR_DATA,
+    MMU_VADDR_INSTRUCTION,
+} mmu_vaddr_t;
+
+/**
+ * External physical memory
+ */
+typedef enum {
+    MMU_TARGET_FLASH0 = BIT(0),
+    MMU_TARGET_PSRAM0 = BIT(1),
+} mmu_target_t;
+
+/**
+ * MMU table id
+ */
+typedef enum {
+    MMU_TABLE_CORE0,
+    MMU_TABLE_CORE1,
+} mmu_table_id_t;
 
 
 #ifdef __cplusplus
