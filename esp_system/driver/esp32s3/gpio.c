5efc2849bf4fa1c91e514116f9fc5e16840860f6
lubin <binidentity@gmail.com>
2023-03-20 18:56:21 +0800

    gpio

--- esp_system/driver/esp32s3/gpio.c
+++ esp_system/driver/esp32s3/gpio.c
@@ -1,13 +1,99 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 #include <sys/errno.h>
+#include <soc/io_mux_reg.h>
+#include <soc/gpio_struct.h>
+#include <soc/gpio_reg.h>
+#include <soc/gpio_sig_map.h>
 
 #include "gpio.h"
 #include "hw/hal/gpio_hal.h"
+
+/***************************************************************************/
+/** @def
+****************************************************************************/
+#define PIN_NB_IS_INVALID(NB)           (NB > 48 || (NB > 21 && pin_nb < 26))
+
+union io_mux_reg_t
+{
+    struct
+    {
+        uint32_t slp_oe     : 1;
+        uint32_t slp_sel    : 1;
+        uint32_t slp_pd     : 1;
+        uint32_t slp_pu     : 1;
+        uint32_t slp_ie     : 1;
+        uint32_t slp_drv    : 2;
+        uint32_t func_pd    : 1;
+        uint32_t func_pu    : 1;
+        uint32_t func_ie    : 1;
+        uint32_t func_drv   : 2;
+        uint32_t func_sel   : 3;
+        uint32_t filter_en  : 1;
+        uint32_t rsv        : 16;
+    };
+
+    uint32_t val;
+};
+typedef union io_mux_reg_t          io_mux_reg_t;
+
+enum    // should mapping => enum GPIO_trig_t in hw/gpio.h
+{
+    PIN_INTR_DISABLE,
+    PIN_INTR_TRIG_RISING_EDGE,
+    PIN_INTR_TRIG_FALLING_EDGE,
+    PIN_INTR_TRIG_BOTH_EDGE,
+    PIN_INTR_TRIG_LOW_LEVEL,
+    PIN_INTR_TRIG_HIGH_LEVEL,
+};
+
+/***************************************************************************/
+/** @internal
+****************************************************************************/
+static io_mux_reg_t *IO_MUX_REG(uint8_t pin_nb);
 
 /***************************************************************************/
 /** constructor
 ****************************************************************************/
 void GPIO_initialize(void)
 {
+}
+
+void GPIO_print_iomux(void)
+{
+    #define PIN_DRV_MA(DRV)         (0 == DRV ? 5 : (1 == DRV ? 10 : (2 == DRV ? 20 : 30)))
+
+    printf("+-----+-----------------------------+-----------------------------+-----+-----+\n");
+    printf("|     | GenericPIO functions        | RTC(sleep) GPIO             |     |     |\n");
+    printf("+ PIN +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+ FLT | SEL |\n");
+    printf("|     | SEL | IE  | PD  | PU  | DRV | OE  | IE  | PD  | PU  | DRV |     |     |\n");
+    printf("+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
+
+
+    for (int i = 0; i < 49; i ++)
+    {
+        if (i < 22 || i > 25)
+        {
+            io_mux_reg_t *reg = IO_MUX_REG(i);
+
+            printf("| %02d  ", i);
+            printf("| %u   | %u   | %u   | %u   |%2d mA", reg->func_sel, reg->func_ie, reg->func_pd, reg->func_pu, PIN_DRV_MA(reg->func_drv));
+
+            if (reg->slp_sel)
+                printf("| %u   | %u   | %u   | %u   |%2d mA", reg->slp_oe, reg->slp_ie, reg->slp_pd, reg->slp_pu, PIN_DRV_MA(reg->slp_drv));
+            else
+                printf("| DISABLED                    ");
+
+            printf("| %u   |\n", reg->filter_en);
+        }
+        else if (22 == i)
+            printf("+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+...22~25\n");
+
+    }
+
+    printf("+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+\n");
+    #undef PIN_DRV_MA
 }
 
 /***************************************************************************/
@@ -44,19 +130,36 @@ int GPIO_hold_repeating(void *const gpio, uint32_t pins, uint32_t millisecond)
 /***************************************************************************/
 /** @implements: configure by PIN number
 ****************************************************************************/
-int GPIO_disable_pin_nb(enum GPIO_resistor_t pull, uint8_t pin_nb)
+int GPIO_disable_pin_nb(enum GPIO_pad_pull_t pull, uint8_t pin_nb)
 {
+    io_mux_reg_t *io_mux_reg = IO_MUX_REG(pin_nb);
+    if (! io_mux_reg)
+        return EINVAL;
 
+    GPIO.pin[pin_nb].int_ena = PIN_INTR_DISABLE;
+
+    if (31 > pin_nb)
+        GPIO.enable_w1tc = (1UL << pin_nb);
+    else
+        GPIO.enable1_w1tc.data = (1UL << (pin_nb - 32));
+
+    io_mux_reg->func_ie = 0;
+    io_mux_reg->func_pd = 0;
+    io_mux_reg->func_pu = 1;        // pull-up to powersave?
 }
 
 int GPIO_setdir_input_pin_nb(uint8_t pin_nb)
 {
-
+    io_mux_reg_t *io_mux_reg = IO_MUX_REG(pin_nb);
+    if (! io_mux_reg)
+        return EINVAL;
 }
 
 int GPIO_setdir_output_pin_nb(enum GPIO_output_mode_t mode, uint8_t pin_nb)
 {
-
+    io_mux_reg_t *io_mux_reg = IO_MUX_REG(pin_nb);
+    if (! io_mux_reg)
+        return EINVAL;
 }
 
 /***************************************************************************/
@@ -64,23 +167,35 @@ int GPIO_setdir_output_pin_nb(enum GPIO_output_mode_t mode, uint8_t pin_nb)
 ****************************************************************************/
 uint32_t GPIO_peek(void *const gpio, uint32_t pins)
 {
-
+    if ((uintptr_t)gpio != (uintptr_t)gpio & pins)
+        return (uint32_t)__set_errno_nullptr(EINVAL);
 }
 
 uint32_t GPIO_peek_output(void *const gpio, uint32_t pins)
 {
+    if ((uintptr_t)gpio != (uintptr_t)gpio & pins)
+        return (uint32_t)__set_errno_nullptr(EINVAL);
 }
 
 void GPIO_toggle(void *const gpio, uint32_t pins)
 {
+    if ((uintptr_t)gpio != (uintptr_t)gpio & pins)
+        return (void)__set_errno_nullptr(EINVAL);
+
 }
 
 void GPIO_set(void *const gpio, uint32_t pins)
 {
+    if ((uintptr_t)gpio != (uintptr_t)gpio & pins)
+        return (void)__set_errno_nullptr(EINVAL);
+
 }
 
 void GPIO_clear(void *const gpio, uint32_t pins)
 {
+    if ((uintptr_t)gpio != (uintptr_t)gpio & pins)
+        return (void)__set_errno_nullptr(EINVAL);
+
 }
 
 /***************************************************************************/
@@ -91,6 +206,27 @@ void GPIO_HAL_intr_enable(void *const gpio, uint32_t pins, enum GPIO_trig_t trig
 }
 
 void GPIO_HAL_intr_disable(void *const gpio, uint32_t pins)
+{
+
+}
+
+/***************************************************************************/
+/** @internal
+****************************************************************************/
+static io_mux_reg_t *IO_MUX_REG(uint8_t pin_nb)
+{
+    if (PIN_NB_IS_INVALID(pin_nb))
+        return NULL;
+    else
+        return (io_mux_reg_t *)(REG_IO_MUX_BASE + (pin_nb + 1) * sizeof(uint32_t));
+}
+
+static void GPIO_input_en(bool en)
+{
+
+}
+
+static void GPIO_ouput_en(bool en, bool opendrain)
 {
 
 }
--- esp_system/driver/esp32s3/include/gpio.h
+++ esp_system/driver/esp32s3/include/gpio.h
@@ -1,59 +1,60 @@
 #ifndef __ESP32S3_GPIO_H
 #define __ESP32S3_GPIO_H                1
 
+#include <features.h>
 #include <hw/gpio.h>
 
-    #define GPIO0                       0   // PIN:  0~21
-    #define GPIO1                       26  // PIN: 26~48
+    #define GPIO_H                      ((void *)1UL << 31) // PIN: 26~48
+    #define GPIO_L                      ((void *)1UL << 30) // PIN:  0~21
 
 // PIN0 ~ PIN21
-    #define PIN0                        (1 << 0)
-    #define PIN1                        (1 << 1)
-    #define PIN2                        (1 << 2)
-    #define PIN3                        (1 << 3)
-    #define PIN4                        (1 << 4)
-    #define PIN5                        (1 << 5)
-    #define PIN6                        (1 << 6)
-    #define PIN7                        (1 << 7)
-    #define PIN8                        (1 << 8)
-    #define PIN9                        (1 << 9)
-    #define PIN10                       (1 << 10)
-    #define PIN11                       (1 << 11)
-    #define PIN12                       (1 << 12)
-    #define PIN13                       (1 << 13)
-    #define PIN14                       (1 << 14)
-    #define PIN15                       (1 << 15)
-    #define PIN16                       (1 << 16)
-    #define PIN17                       (1 << 17)
-    #define PIN18                       (1 << 18)
-    #define PIN19                       (1 << 19)
-    #define PIN20                       (1 << 20)
-    #define PIN21                       (1 << 21)
+    #define PIN0                        ((uintptr_t)GPIO_L | (1 << 0))
+    #define PIN1                        ((uintptr_t)GPIO_L | (1 << 1))
+    #define PIN2                        ((uintptr_t)GPIO_L | (1 << 2))
+    #define PIN3                        ((uintptr_t)GPIO_L | (1 << 3))
+    #define PIN4                        ((uintptr_t)GPIO_L | (1 << 4))
+    #define PIN5                        ((uintptr_t)GPIO_L | (1 << 5))
+    #define PIN6                        ((uintptr_t)GPIO_L | (1 << 6))
+    #define PIN7                        ((uintptr_t)GPIO_L | (1 << 7))
+    #define PIN8                        ((uintptr_t)GPIO_L | (1 << 8))
+    #define PIN9                        ((uintptr_t)GPIO_L | (1 << 9))
+    #define PIN10                       ((uintptr_t)GPIO_L | (1 << 10))
+    #define PIN11                       ((uintptr_t)GPIO_L | (1 << 11))
+    #define PIN12                       ((uintptr_t)GPIO_L | (1 << 12))
+    #define PIN13                       ((uintptr_t)GPIO_L | (1 << 13))
+    #define PIN14                       ((uintptr_t)GPIO_L | (1 << 14))
+    #define PIN15                       ((uintptr_t)GPIO_L | (1 << 15))
+    #define PIN16                       ((uintptr_t)GPIO_L | (1 << 16))
+    #define PIN17                       ((uintptr_t)GPIO_L | (1 << 17))
+    #define PIN18                       ((uintptr_t)GPIO_L | (1 << 18))
+    #define PIN19                       ((uintptr_t)GPIO_L | (1 << 19))
+    #define PIN20                       ((uintptr_t)GPIO_L | (1 << 20))
+    #define PIN21                       ((uintptr_t)GPIO_L | (1 << 21))
 
 // PIN26 ~ PIN48
-    #define PIN26                       (1 << 26)
-    #define PIN27                       (1 << 27)
-    #define PIN28                       (1 << 28)
-    #define PIN29                       (1 << 29)
-    #define PIN30                       (1 << 30)
-    #define PIN31                       (1 << 31)
-    #define PIN32                       (1 << 32)
-    #define PIN33                       (1 << 33)
-    #define PIN34                       (1 << 34)
-    #define PIN35                       (1 << 35)
-    #define PIN36                       (1 << 36)
-    #define PIN37                       (1 << 37)
-    #define PIN38                       (1 << 38)
-    #define PIN39                       (1 << 39)
-    #define PIN40                       (1 << 40)
-    #define PIN41                       (1 << 41)
-    #define PIN42                       (1 << 42)
-    #define PIN43                       (1 << 43)
-    #define PIN44                       (1 << 44)
-    #define PIN45                       (1 << 45)
-    #define PIN46                       (1 << 46)
-    #define PIN47                       (1 << 47)
-    #define PIN48                       (1 << 48)
+    #define PIN26                       ((uintptr_t)GPIO_H | 1 << 0)
+    #define PIN27                       ((uintptr_t)GPIO_H | 1 << 1)
+    #define PIN28                       ((uintptr_t)GPIO_H | 1 << 2)
+    #define PIN29                       ((uintptr_t)GPIO_H | 1 << 3)
+    #define PIN30                       ((uintptr_t)GPIO_H | 1 << 4)
+    #define PIN31                       ((uintptr_t)GPIO_H | 1 << 5)
+    #define PIN32                       ((uintptr_t)GPIO_H | 1 << 6)
+    #define PIN33                       ((uintptr_t)GPIO_H | 1 << 7)
+    #define PIN34                       ((uintptr_t)GPIO_H | 1 << 8)
+    #define PIN35                       ((uintptr_t)GPIO_H | 1 << 9)
+    #define PIN36                       ((uintptr_t)GPIO_H | 1 << 10)
+    #define PIN37                       ((uintptr_t)GPIO_H | 1 << 11)
+    #define PIN38                       ((uintptr_t)GPIO_H | 1 << 12)
+    #define PIN39                       ((uintptr_t)GPIO_H | 1 << 13)
+    #define PIN40                       ((uintptr_t)GPIO_H | 1 << 14)
+    #define PIN41                       ((uintptr_t)GPIO_H | 1 << 15)
+    #define PIN42                       ((uintptr_t)GPIO_H | 1 << 16)
+    #define PIN43                       ((uintptr_t)GPIO_H | 1 << 17)
+    #define PIN44                       ((uintptr_t)GPIO_H | 1 << 18)
+    #define PIN45                       ((uintptr_t)GPIO_H | 1 << 19)
+    #define PIN46                       ((uintptr_t)GPIO_H | 1 << 20)
+    #define PIN47                       ((uintptr_t)GPIO_H | 1 << 21)
+    #define PIN48                       ((uintptr_t)GPIO_H | 1 << 22)
 
     #define GPIO0_VALID_MASK            ((1 << 22) - 1)
     #define GPIO1_VALID_MASK            ((1 << (48 - 26 + 1)) - 1)
@@ -61,11 +62,18 @@
     // default pull-up when gpio is disabled for powersave
     #define GPIO_DISABLE_PULL           (PULL_UP)
 
+__BEGIN_DECLS
+/***************************************************************************/
+/**  GPIO io mux table console
+****************************************************************************/
+extern __attribute__((nothrow, nonnull))
+    void GPIO_print_iomux(void);
+
 /***************************************************************************/
 /**  GPIO configure by PIN number
 ****************************************************************************/
 extern __attribute__((nothrow, nonnull))
-    int GPIO_disable_pin_nb(enum GPIO_resistor_t pull, uint8_t pin_nb);
+    int GPIO_disable_pin_nb(enum GPIO_pad_pull_t pull, uint8_t pin_nb);
 
 extern __attribute__((nothrow, nonnull))
     int GPIO_setdir_input_pin_nb(uint8_t pin_nb);
@@ -73,4 +81,5 @@ extern __attribute__((nothrow, nonnull))
 extern __attribute__((nothrow, nonnull))
     int GPIO_setdir_output_pin_nb(enum GPIO_output_mode_t mode, uint8_t pin_nb);
 
+__END_DECLS
 #endif
