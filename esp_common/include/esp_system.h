f3145c2590c073532a40bfd73bf31bab95011d4e
lubin <binidentity@gmail.com>
2023-03-31 18:59:50 +0800

    misc

--- esp_common/include/esp_system.h
+++ esp_common/include/esp_system.h
@@ -16,10 +16,10 @@ extern __attribute__((noreturn))
 
     /**
      *  this function will called before enter main()
-     *  NOTE: rtos context here must be initialized
+     *      NOTE: rtos context here must be initialized
     */
 extern __attribute__((nothrow))
-    void __esp_rtos_initialize(void);
+    void _esp_rtos_start(void);
 
 /****************************************************************************
  *  for esp-idf's freertos porting
@@ -28,26 +28,6 @@ extern __attribute__((nothrow))
     esp_err_t esp_register_shutdown_handler(void (*func_ptr)(void));
 extern __attribute__((nothrow))
     esp_err_t esp_unregister_shutdown_handler(void (*func_ptr)(void));
-
-/*
-typedef enum
-{
-    ESP_RST_UNKNOWN,
-    ESP_RST_POWERON,
-    ESP_RST_EXT,
-    ESP_RST_SW,
-    ESP_RST_PANIC,
-    ESP_RST_INT_WDT,
-    ESP_RST_TASK_WDT,
-    ESP_RST_WDT,
-    ESP_RST_DEEPSLEEP,
-    ESP_RST_BROWNOUT,
-    ESP_RST_SDIO,
-} esp_reset_reason_t;
-
-extern __attribute__((nothrow, const))
-    esp_reset_reason_t esp_reset_reason(void);
-*/
 
 struct __esp_init_fn
 {
@@ -61,6 +41,6 @@ struct __esp_init_fn
         struct __esp_init_fn esp_system_init_fn_##f = { .fn = ( __esp_system_init_fn_##f), .cores = (c) };  \
     static esp_err_t __esp_system_init_fn_##f(void)
 
-#define ESP_SYSTEM_INIT_ALL_CORES ((1 << SOC_CPU_CORES_NUM) - 1)
+#define ESP_SYSTEM_INIT_ALL_CORES ((1U << SOC_CPU_CORES_NUM) - 1)
 
 __END_DECLS
